d696d867c7257beaa4bb5d631d62a43775307369
1000zoo <cjswldn99@gmail.com>
2021-12-16 23:50:07 +0900

    leetcode linked list / remove linked list elements

---
+++ leetcode/linked_list/linked_list_0203/src/Solution.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/remove-linked-list-elements/submissions/
+//2021-12-16
+
+public class Solution {
+
+    //Runtime: 1 ms, faster than 74.30% of Java online submissions for Remove Linked List Elements.
+    //Memory Usage: 46.3 MB, less than 5.26% of Java online submissions for Remove Linked List Elements.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode curr = dummy;
+        while(curr.next != null){
+            if(curr.next.val == val) curr.next = curr.next.next;
+            else curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
